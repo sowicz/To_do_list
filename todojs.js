eab86813fcf8a0544b593897a38e51b9c038145d
Kamil So <sowicz09@gmail.com>
2022-01-16 16:23:01 +0100

    Pełna działająca funkcja filtrowania
    wystarczyło dodać  do 'niezrobione' instrukcji warunkowej przedrostek '!' przed zmienną x i działa

--- todojs.js
+++ todojs.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 const filtrOpcja = document.querySelector('.filtrowanie');
 
 // eventy listeners
+// document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', koszzrobione);
 filtrOpcja.addEventListener('click', filtrTodo);
@@ -25,6 +26,9 @@ function addTodo(event) {
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
   
+    // add todo to local storage
+   // saveLocalTodos(todoInput.value);
+
     // zrobione - przycisk
     const zrobioneButton = document.createElement('button');
     zrobioneButton.innerHTML = '<i class="fas fa-check"></i>'
@@ -79,17 +83,77 @@ function filtrTodo(c) {
                    x.style.display = 'flex'; 
                 }
                 else {
-                    x.style.display = 'none'
+                    x.style.display = 'none';
                 }
-            /*case "niezrobione":
-                if(x.classList.contains('todo')){
+                break;
+            case "niezrobione":
+                if(!x.classList.contains('completed')){
                     x.style.display = 'flex';
                 }
                 else {
-                    x.style.display = 'none'
-                }*/
+                    x.style.display = 'none';
+                } 
+                break;
             }
         })
     }
+/*
+function saveLocalTodos(todo) {
+    //check -- czy mamy juz jakies zadania tutaj?
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+        todos.push(todo);
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
 
 
+function getTodos () {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo){
+            // todo DIV - make new todo div
+            const todoDiv = document.createElement('div');
+            todoDiv.classList.add("todo");
+  
+            // create LI - 
+            const newTodo = document.createElement('li');
+            newTodo.innerText = todo;
+            newTodo.classList.add('todo-item');
+            todoDiv.appendChild(newTodo);
+        
+            // add todo to local storage
+            saveLocalTodos(todoInput.value);
+
+            // zrobione - przycisk
+            const zrobioneButton = document.createElement('button');
+            zrobioneButton.innerHTML = '<i class="fas fa-check"></i>'
+            zrobioneButton.classList.add("zrobione-btn")
+            todoDiv.appendChild(zrobioneButton);
+
+            // kosz - przycisk
+            const koszButton = document.createElement('button');
+            koszButton.innerHTML = '<i class="fas fa-trash"></i>'
+            koszButton.classList.add("kosz-btn")
+            todoDiv.appendChild(koszButton);
+           
+            // append to list
+            todoList.appendChild(todoDiv);
+
+    })
+}
+
+function removeLocalStorage
+
+
+----- 1h:00 ------
+*/
